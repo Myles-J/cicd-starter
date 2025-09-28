5d08d7b1d238ead3ce2bc16527ee2fc6b32ff48e
Myles Jefferson <99mylesjefferson@gmail.com>
2025-09-28 17:15:24 -0600

    chore: format

--- json.go
+++ json.go
@@ -32,7 +32,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 
-	if (err != nil) {
+	if err != nil {
 		log.Printf("Error writing data: %s", err)
 		return
 	}
